fe9ddd48fe9f8a048af970f35dcf6d5411d94d3e
umair-dev55 <umair.personal55@gmail.com>
2026-02-22 18:40:04 +0500

    timeline

--- app/page.js
+++ app/page.js
@@ -3,46 +3,57 @@ import { useEffect } from "react";
 import gsap from "gsap";
 const page = () => {
   useEffect(() => {
-    gsap.from("#box", {
-      x: 350,
-      opacity: 1,
+    var tl = gsap.timeline();
+    tl.from(".card1", {
+      x: 1200,
       duration: 1,
-      delay: 2,
     });
-    gsap.to("#box1", {
-      x: 350,
-      opacity: 1,
+    tl.from(".card2", {
       duration: 1,
-      delay: 2,
+      x: 1200,
     });
+
+    tl.from(".card3", {
+      duration: 1,
+      x: 1200,
+    });
+
+    // gsap.to("h1", {
+    //   y: -20,
+    //   color: "gray",
+    //   stagger: 1,
+    //   duration: 0.2,
+    // });
+    // gsap.from(".card", {
+    //   y: 40,
+    //   repeat: -1,
+    //   duration: 0.4,
+    //   yoyo: true,
+    // });
   });
   return (
-    <div className="h-screen">
-      <section className="p-10">
+    <div className="h-full p-10">
+      <section className="">
+        {/* Timeline  */}
+        <div className=" p-10  ">
+          <div className="card1 p-2 bg-gray-800 size-30">One</div>
+          <div className="card2 p-2 bg-gray-700 size-30">Two</div>
+          <div className="card3 p-2 bg-gray-700 size-30">Three</div>
+        </div>
+        {/* To & From  */}
         <div
           id="box"
-          className="size-10 rounded-full bg-cyan-700  text-center "
+          className="w-100 border -2 border-gray-700  text-center"
         ></div>
-        <div
-          id="box1"
-          className="size-10 bg-cyan-700 rounded-full text-center "
-        ></div>
-        <div
-          id="box2"
-          className="size-10 bg-cyan-700 rounded-full text-center "
-        ></div>
-        <div
-          id="box3"
-          className="size-10 bg-cyan-700 rounded-full text-center "
-        ></div>
-        <div
-          id="box4"
-          className="size-10 bg-cyan-700 rounded-full text-center "
-        ></div>
-        <div
-          id="box5"
-          className="size-10 bg-cyan-700 rounded-full text-center "
-        ></div>
+        <div>
+          <div id="para" className="text-2xl py-20 ">
+            <h1>lorem lorem lorem lorem lorem lorem</h1>
+            <h1>lorem lorem lorem lorem lorem lorem</h1>
+            <h1>lorem lorem lorem lorem lorem lorem</h1>
+            <h1>lorem lorem lorem lorem lorem lorem</h1>
+            <h1>lorem lorem lorem lorem lorem lorem</h1>
+          </div>
+        </div>
       </section>
     </div>
   );
