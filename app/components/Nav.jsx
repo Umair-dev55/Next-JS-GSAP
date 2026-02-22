d5681c84b6d865ad286b473f408c47e79fa4f4d1
umair-dev55 <umair.personal55@gmail.com>
2026-02-22 21:24:00 +0500

    add next js Routing included  timeline mastery

---
+++ app/components/Nav.jsx
@@ -0,0 +1,64 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+import { useEffect } from "react";
+import gsap from "gsap";
+
+const Nav = () => {
+  useEffect(() => {
+    var tl = gsap.timeline();
+    tl.from("#card1", {
+      x: 1200,
+      duration: 1,
+    });
+    tl.from("#card2", {
+      duration: 1,
+      x: 1200,
+    });
+
+    tl.from("#card3", {
+      duration: 1,
+      x: 1200,
+    });
+    var timeline = gsap.timeline();
+    timeline.from(".logo", {
+      y: -40,
+      delay: 0.5,
+      duration: 0.2,
+      opacity: 0,
+    });
+    timeline.from(".nav-link", {
+      opacity: 0,
+      y: -40,
+      duration: 0.5,
+    });
+    timeline.from("#hero", {
+      opacity: 0,
+      x: -40,
+      duration: 0.5,
+    });
+  });
+  return (
+    <div>
+      <nav className="justify-between bg-black/40 p-10 text-[16px]">
+        <h3 className="logo">Umair</h3>
+        <div className=" flex gap-10">
+          <Link className="nav-link" href="/">
+            home
+          </Link>
+          <Link className="nav-link" href="/product">
+            Product
+          </Link>
+          <Link className="nav-link" href="/contact">
+            contact
+          </Link>
+          <Link className="nav-link" href="/about">
+            About
+          </Link>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Nav;
--- app/page.js
+++ app/page.js
@@ -1,90 +1,33 @@
-"use client";
-import { useEffect } from "react";
-import gsap from "gsap";
+
 const page = () => {
-  useEffect(() => {
-    var tl = gsap.timeline();
-    tl.from("#card1", {
-      x: 1200,
-      duration: 1,
-    });
-    tl.from("#card2", {
-      duration: 1,
-      x: 1200,
-    });
-
-    tl.from("#card3", {
-      duration: 1,
-      x: 1200,
-    });
-    var timeline = gsap.timeline();
-    timeline.from(".logo", {
-      y: -40,
-      delay: 0.5,
-      duration: 0.2,
-      opacity: 0,
-    });
-    timeline.from("#nav", {
-      opacity: 0,
-      y: -40,
-      duration: 0.5,
-    });
-    timeline.from("#hero", {
-      opacity: 0,
-      x: -40,
-      duration: 0.5,
-    });
-
-    // gsap.to("h1", {
-    //   y: -20,
-    //   color: "gray",
-    //   stagger: 1,
-    //   duration: 0.2,
-    // });
-    // gsap.from(".card", {
-    //   y: 40,
-    //   repeat: -1,
-    //   duration: 0.4,
-    //   yoyo: true,
-    // });
-  });
+ 
   return (
     <div className="">
-      {/* Nav Here  */}
-      <nav className="justify-between bg-black/40 p-10 text-[16px]">
-        <h3 className="logo">Umair</h3>
-        <div className=" flex gap-10">
-          <a id="nav" href="#">
-            Home
-          </a>
-          <a id="nav" href="#">
-            Products
-          </a>
-          <a id="nav" href="#">
-            Services
-          </a>
-          <a id="nav" href="#">
-            About
-          </a>
-          <a id="nav" href="#">
-            Contact
-          </a>
-        </div>
-      </nav>
+      {/* hero section  */}
       <div className="h-screen">
         <h1 id="hero" className="text-2xl abs-center">
           Coding ERA
         </h1>
       </div>
+      {/* cards  */}
       <div className="relative p-10  ">
         <div className="*:m-5 sm:flex sm:justify-center">
-          <div id="card1" className="bg-black size-70 rounded-2xl ">
+          <div
+            id="card1"
+            className="bg-black size-70 rounded-2xl justify-center "
+          >
             Card
           </div>
-          <div id="card2" className="bg-black size-70 rounded-2xl ">
+          <div
+            id="card2"
+            className="bg-black size-70 rounded-2xl justify-center"
+          >
             Card
           </div>
-          <div id="card3" className="bg-black size-70 rounded-2xl ">
+          <div
+            id="card3"
+            className="bg-black size-70 rounded-2xl justify-center"
+          >
             Card
           </div>
         </div>
